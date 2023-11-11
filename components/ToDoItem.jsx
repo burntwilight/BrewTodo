07a73380da8fb4a37faca9ec3e29aa4f65d0b9a9
Dakota Bradford <dakotagreybradford@gmail.com>
2023-11-10 17:56:23 -0700

    Added animation effect to input tasks

--- components/ToDoItem.jsx
+++ components/ToDoItem.jsx
@@ -2,7 +2,7 @@ import CompleteTaskButton from "./CompleteTaskButton"
 
 const ToDoItem = ( { item } ) => {
   return (
-    <div className="shadow-sm shadow-text w-full border border-text rounded-xl flex flex-center items-center p-4 m-4 min-w-[30vw]">
+    <div className="animate-in shadow-sm shadow-text w-full border border-text rounded-xl flex flex-center items-center p-4 m-4 min-w-[30vw]">
       <CompleteTaskButton id={item.id} />
       <p className="text-text m-4">{item.task !== undefined && item.task !== '' ? item.task : ''}</p>
     </div>
